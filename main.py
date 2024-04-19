f0e86d2bf392fc4085b9fbf4bc544f46d8283598
ak47andrew <ak47reborntumpa@gmail.com>
2024-04-19 21:43:25 +0300

    all levels loading

--- main.py
+++ main.py
@@ -6,14 +6,23 @@ headers = {
 }
 
 data = {
-    "levelID": 3915,
+    "levelID": 1,
     "secret": "Wmfd2893gb7"
 }
 
-req = requests.post("https://gmd.plusgdps.dev/database/downloadGJLevel22.php", data=data, headers=headers)
-data = sorted(itertools.batched(req.text.split(":"), n=2), key=lambda x: int(x[0]))
-dict_data = {int(x[0]): x[1] for x in data}
-for i in dict_data:
-    if i == 4:
-        continue
-    print(i, dict_data[i])
+while data["levelID"] <= 3924:
+    print(f"Loading level with ID: {data['levelID']}...")
+    req = requests.post("https://gmd.plusgdps.dev/database/downloadGJLevel22.php", data=data, headers=headers)
+    if not req:
+        print("Request error!")
+        print(f"{req.status_code=}\n{req.text=}")
+        break
+    if req.text == "-1":
+        print("404: Level not found")
+    else:
+        with open("levels.txt", "a") as f:
+            f.write(req.text)
+            f.write("\n")
+        print(f"Level found and saved! Name: {req.text.split(':')[3]}")
+    print("Done!")
+    data["levelID"] += 1
