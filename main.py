e667f15f5cd3629077cfcd9dbaf7e33aaa7afd4d
ak47andrew <ak47reborntumpa@gmail.com>
2024-04-20 20:32:32 +0300

    plots

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-import level
-import pymongo
+import json
+import matplotlib.pyplot as plt
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["test"]
-mycol = mydb["test"]
+levels = json.load(open("levels.json"))["levels"]
 
 
-def do_things(string: str):
-    lev = level.Level.from_string(string)
-    lev.levelString = ""
-    mycol.insert_one(lev.to_json())
-    print(f"Level {lev.levelName} was dumped")
+def bubble_sort(a, b):
+    running = True
+    while running:
+        running = False
+        for i in range(len(a) - 1):
+            if a[i] > a[i + 1]:
+                a[i], a[i + 1] = a[i + 1], a[i]
+                b[i], b[i + 1] = b[i + 1], b[i]
+                running = True
+    return a, b
 
 
-with open("levels.txt") as f:
-    for line in f.readlines():
-        do_things(line)
+def plot_levels(metric: str, filename: str):
+    data = list(map(lambda x: x[metric], levels))
+
+    labels = tuple(set(data))
+    sizes = [data.count(x) for x in labels]
+    sizes, labels = bubble_sort(sizes, list(labels))
+    labels = tuple(map(str, labels))
+
+    fig, ax = plt.subplots()
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%')
+    plt.savefig(filename)
+
+
+for key in levels[0].keys():
+    print(f"Starting {key}")
+    plot_levels(key, f"output\\{key}.png")
+    print(f"Done {key}")
